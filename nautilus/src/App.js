952f9535d5629501852b89696bc68f3532ae4989
Victoria Jones <66696620+toristuff97@users.noreply.github.com>
2020-07-27 22:17:59 -0400

    added footer rcc

--- nautilus/src/App.js
+++ nautilus/src/App.js
@@ -1,13 +1,15 @@
-import React from 'react';
+import React, {Component} from 'react';
 import './App.css';
 import Footer from './Components/Footer'
 
-function App() {
+class App extends React.Component {
+  render() {
   return (
     <div className="App">
       <Footer />
     </div>
-  );
+  )
+  }
 }
 
 export default App;
